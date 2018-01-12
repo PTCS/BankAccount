6e461cb023cd525c55d01b703a47f405e7b8847a
Ryan Ritz <rritz@parktudor.org>
2018-01-12 12:46:39 -0500

    Create BankAccount

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount() {
+        balance = 0;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
